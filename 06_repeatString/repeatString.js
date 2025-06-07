43b73e86acd8ae03253fd1646c45b29c835b9556
frixc7 <frixc7@gmail.com>
2025-06-07 01:29:52 +0100

    Solve 06 and 07

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num > 0) {
+        let arr = [];
+        for (let i = 1; i <= num; i++) {
+            arr.push(string);
+        }
+        return arr.join("");
+    } else if (num == 0) {
+        return "";
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,20 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arr = string.split("");
 
+    let left = 0;
+    let right = arr.length - 1;
+    let permuted;
+
+    while (left <= right) {
+        permuted = arr[left];
+        arr[left] = arr[right];
+        arr[right] = permuted;
+
+        left += 1;
+        right -= 1;
+    }
+
+    return arr.join("");
 };
 
 // Do not edit below this line
