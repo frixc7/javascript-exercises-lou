d75e3c977f9478049ae4fa1cdd0beee6089fa705
frixc7 <frixc7@gmail.com>
2025-06-07 01:54:36 +0100

    Solve 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    const NOT_NEGATIVE = a >= 0 && b >= 0;
+    const IS_NUMBER = typeof(a) == "number" && typeof(b) == "number";
+    const IS_INTEGER = a % 1 == 0 && b % 1 == 0;
 
+
+    if (NOT_NEGATIVE && IS_NUMBER && IS_INTEGER) {
+        const min = a < b ? a : b;
+        const max = a > b ? a : b;
+
+        let sum = 0;
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
