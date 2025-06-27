8bd961387853a2684bcfa6bec0164f2009ebf19b
frixc7 <frixc7@gmail.com>
2025-06-27 03:52:35 +0100

    Solve

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // parsing
+    str = str.replace(/[!?, .]/g, '').toLowerCase();
 
+    // checking
+    let i = 0;
+    let j = str.length - 1;
+    while (i < j) {
+        if (str[i] != str[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 };
+
+palindromes("racecar!")
 
 // Do not edit below this line
 module.exports = palindromes;
