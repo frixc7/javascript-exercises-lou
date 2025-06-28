dd659ab3876e4168de6261b7489545746c679f3e
frixc7 <frixc7@gmail.com>
2025-06-28 18:46:24 +0100

    Solve

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const CURRENT_YEAR = new Date().getFullYear();
 
+    let max = 0;
+    let temp;
+    let person; // current person
+    let oldest;
+    
+    for (let i = 0; i < people.length; i++) {
+        person = people[i];
+        if (person.yearOfDeath) {
+            temp = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            temp = CURRENT_YEAR - person.yearOfBirth;
+        }
+        
+        if (temp > max) {
+            max = temp;
+            oldest = person;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
