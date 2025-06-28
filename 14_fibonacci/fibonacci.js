67c61ed445b7142a53331a46460c3b7c0cfa0f07
frixc7 <frixc7@gmail.com>
2025-06-28 18:26:06 +0100

    Solve

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else if (n < 0) {
+        return "OOPS";
+    } else {
+        let x = 1;
+        let y = 0;
+        let fib;
+        for (let i = 1; i < n; i++) {
+            fib = x+y;
+            y = x;
+            x = fib;
+        }
+        return fib;
+    }
+    
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
